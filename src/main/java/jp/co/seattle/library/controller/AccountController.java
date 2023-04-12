dd9e200e3037a1ab2504cfa739de3c5b81315154
tomo <tomo@localhost>
2023-04-12 12:53:24 +0900

    task1_最新

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -48,7 +48,7 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）    public static boolean collectPassward(String )
-		if (password.length() >= 8 && password.matches("[0-9a-zA-Z]+$")) {
+		if (password.length() >= 8 && password.matches("^[0-9a-zA-Z]+$")) {
 			if (password.equals(passwordForCheck)) {
 				// パラメータで受け取ったアカウント情報をDtoに格納する。
 				UserInfo userInfo = new UserInfo();
@@ -57,12 +57,13 @@ public class AccountController {
 				usersService.registUser(userInfo);
 				return "redirect:/login";
 			} else {
-				model.addAttribute("errorMassage", "半角英数字8文字以上で入力してください。");
+				model.addAttribute("errorMessage", "パスワードが一致しません。");
 				return "createAccount";
 
 			}
-		} else
-			model.addAttribute("errorMassage", "確認用パスワードと一致しません。");
+		} else {
+			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください。");
 		return "createAccount";
+		}
 	}
 }
